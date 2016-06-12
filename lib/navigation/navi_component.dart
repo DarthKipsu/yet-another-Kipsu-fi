bbb41bfc0b7aa6ec2f1b8d38ab7c99e2f14f6a52
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-12 17:21:11 +0300

    add empty side navigation component

---
+++ lib/navigation/navi_component.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'side-navigation',
+    templateUrl: 'navi_component.html',
+    styleUrls: const ['navi_component.css'])
+class NaviComponent {}
--- web/main.dart
+++ web/main.dart
@@ -2,8 +2,10 @@ import 'package:angular2/platform/browser.dart';
 
 import 'package:kipsu_fi/app_component.dart';
 import 'package:kipsu_fi/front_page/unicorns_component.dart';
+import 'package:kipsu_fi/navigation/navi_component.dart';
 
 void main() {
   bootstrap(AppComponent);
   bootstrap(UnicornsComponent);
+  bootstrap(NaviComponent);
 }
