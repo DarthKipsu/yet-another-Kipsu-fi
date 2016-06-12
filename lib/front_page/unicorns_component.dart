a5d6eacf426ce770569fa55ebc9eab31609b66a8
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-12 23:56:47 +0300

    add titile for front page

--- lib/front_page/unicorns_component.dart
+++ lib/front_page/unicorns_component.dart
@@ -1,7 +1,18 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+import 'package:kipsu_fi/services/title_service.dart';
 
 @Component(
     selector: 'unicorns',
     templateUrl: 'unicorns_component.html',
     styleUrls: const ['unicorns_component.css'])
-class UnicornsComponent {}
+class UnicornsComponent implements OnActivate {
+  final TitleService _titleService;
+
+  UnicornsComponent(this._titleService);
+
+  routerOnActivate(next, prev) {
+    _titleService.title = "darth.kipsu.fi";
+  }
+}
