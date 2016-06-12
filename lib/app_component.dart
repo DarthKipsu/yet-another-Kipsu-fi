c8d8438eba8559be204300c94df8b20440c14cb9
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-12 18:43:53 +0300

    move directive list to app component from main.dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,13 @@
 import 'package:angular2/core.dart';
 
+import 'package:kipsu_fi/front_page/unicorns_component.dart';
+import 'package:kipsu_fi/navigation/navi_component.dart';
+
 @Component(
     selector: 'app',
-    templateUrl: 'app_component.html')
+    templateUrl: 'app_component.html',
+    directives: const [
+      NaviComponent,
+      UnicornsComponent
+    ])
 class AppComponent {}
