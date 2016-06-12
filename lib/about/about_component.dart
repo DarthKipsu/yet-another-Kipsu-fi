41c6048870dd8755ede2f30559e97f81fc5cd8ac
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-12 19:28:40 +0300

    add navigation with angular router

---
+++ lib/about/about_component.dart
@@ -0,0 +1,8 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'about-me',
+    templateUrl: 'about_component.html',
+    styleUrls: const ['about_component.css'])
+class AboutComponent {}
+
--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,24 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 
+import 'package:kipsu_fi/about/about_component.dart';
 import 'package:kipsu_fi/front_page/unicorns_component.dart';
-import 'package:kipsu_fi/navigation/navi_component.dart';
 
 @Component(
     selector: 'app',
     templateUrl: 'app_component.html',
+    styleUrls: const ['app_component.css'],
     directives: const [
-      NaviComponent,
+      ROUTER_DIRECTIVES,
+      AboutComponent,
       UnicornsComponent
-    ])
+    ],
+    providers: const [ROUTER_PROVIDERS])
+
+@RouteConfig(const [
+    const Route(path: '/', name: 'Unicorns', component: UnicornsComponent, useAsDefault: true),
+    const Route(path: '/about', name: 'About', component: AboutComponent)
+])
+
 class AppComponent {}
+
