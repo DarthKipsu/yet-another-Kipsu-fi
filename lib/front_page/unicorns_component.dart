9604d55f4e3f75c72b8cdf83576386d5e8fd15e9
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-12 10:56:40 +0300

    add styles for unicorns to make them better responsive

--- lib/front_page/unicorns_component.dart
+++ lib/front_page/unicorns_component.dart
@@ -2,5 +2,6 @@ import 'package:angular2/core.dart';
 
 @Component(
     selector: 'unicorns',
-    templateUrl: 'unicorns_component.html')
+    templateUrl: 'unicorns_component.html',
+    styleUrls: const ['unicorns_component.css'])
 class UnicornsComponent {}
