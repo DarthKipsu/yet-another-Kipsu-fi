32887acfcb2cd831a901f228f8598830453470e8
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-13 23:55:02 +0300

    add about page with original contents

--- lib/about/about_component.dart
+++ lib/about/about_component.dart
@@ -8,6 +8,17 @@ import 'package:kipsu_fi/services/title_service.dart';
     templateUrl: 'about_component.html',
     styleUrls: const ['about_component.css'])
 class AboutComponent implements OnActivate {
+  final String coverPicUrl = '/packages/kipsu_fi/assets/about.jpg';
+  final String name = 'Verna Koskinen';
+  final String username = 'DarthKipsu';
+  final String email = 'darth.kipsu@gmail.com';
+  final String phone = '+358 45 1277921';
+  final String address = 'Susitie 3 a 4, 00800, Helsinki, Finland';
+  final String website = 'darth.kipsu.fi';
+  final String github = 'github.com/DarthKipsu';
+  final String linkedin = 'linkedin.com/in/vernakoskinen';
+  final String facebook = 'facebook.com/verna.koskinen';
+
   final TitleService _titleService;
 
   AboutComponent(this._titleService);
@@ -15,6 +26,10 @@ class AboutComponent implements OnActivate {
   routerOnActivate(next, prev) {
     _titleService.title = "About Me";
     _titleService.subtitle = "Find out more about who I am.";
+  }
+
+  String getSrcFor(String link) {
+    return '//$link';
   }
 }
 
