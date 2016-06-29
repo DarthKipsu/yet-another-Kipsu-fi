0029f828716834356b1a00b13af7ae1b6dffe04d
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-29 22:45:35 +0300

    small improvements for world component

--- lib/about/world_component.dart
+++ lib/about/world_component.dart
@@ -34,13 +34,12 @@ class WorldComponent implements OnInit {
     if (country != null) {
       final days = country.visited.fold(0,
           (prevValue, c) => prevValue + c['left'].difference(c['arrived']).inDays);
-      if (days < 1) return 'less than one day';
+      if (days < 1) return 'less than a day';
       if (days < 7) return '$days days';
       if (days < 365) {
         final weeks = (days / 7).floor();
         final extraDays = days % 7;
-        if (extraDays == 0) return '$weeks weeks';
-        return '$weeks weeks and $extraDays days';
+        return extraDays == 0 ? '$weeks weeks' : '$weeks weeks and $extraDays days';
       }
       final years = (days / 365).floor();
       final weeks = (days % 365 / 7).floor();
