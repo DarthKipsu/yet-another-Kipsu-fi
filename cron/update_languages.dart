d1ddf8409e88dd1b2da06c97e745a25e5754aa18
Verna Koskinen <darth.kipsu@gmail.com>
2016-06-30 23:52:04 +0300

    print repository list

--- cron/update_languages.dart
+++ cron/update_languages.dart
@@ -10,9 +10,18 @@ void main() {
           "token ${Platform.environment['KIPSUFI_GITHUB_TOKEN']}");
       return request.close();
     })
-    .then((HttpClientResponse response) {
-      response.transform(UTF8.decoder).listen((content) => print(content));
-      httpClient.close();
-    });
+  .then((HttpClientResponse response) {
+    final contents = new StringBuffer();
+    response.transform(UTF8.decoder).listen((String data) => contents.write(data),
+        onDone: () => onRepositoriesLoaded(contents.toString()));
+    httpClient.close();
+  });
+}
+
+void onRepositoriesLoaded(String response) {
+  final data = JSON.decode(response);
+  data.forEach((d) {
+    print('https://api.github.com/repos/${d['full_name']}/languages');
+  });
 }
 
