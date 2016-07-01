76c65e6071ef00d276587639d3744c6da363aa0c
Verna Koskinen <darth.kipsu@gmail.com>
2016-07-02 00:09:52 +0300

    read cronjob data from file

--- lib/about/my_languages_component.dart
+++ lib/about/my_languages_component.dart
@@ -9,8 +9,23 @@ import 'package:angular2/router.dart';
     templateUrl: 'my_languages_component.html',
     styleUrls: const ['my_languages_component.css'])
 class MyLanguagesComponent implements OnInit {
+  final httpRequest = new HttpRequest();
 
   ngOnInit() {
+    final path = 'packages/kipsu_fi/cron/languages';
+    httpRequest
+      ..open('GET', path)
+      ..onLoadEnd.listen((_) => onLanguagesLoaded())
+      ..send('');
+  }
+
+  void onLanguagesLoaded() {
+    if (httpRequest.status == 200) {
+      final data = JSON.decode(httpRequest.responseText);
+      data.forEach((k, v) {
+        print('$k: $v');
+      });
+    }
   }
 }
 
