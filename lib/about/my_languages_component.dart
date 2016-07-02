9eac964b994896221a0b1b22d9dd2adbe359c419
Verna Koskinen <darth.kipsu@gmail.com>
2016-07-02 11:58:13 +0300

    display language and bytes in GitHub

--- lib/about/my_languages_component.dart
+++ lib/about/my_languages_component.dart
@@ -9,22 +9,36 @@ import 'package:angular2/router.dart';
     templateUrl: 'my_languages_component.html',
     styleUrls: const ['my_languages_component.css'])
 class MyLanguagesComponent implements OnInit {
-  final httpRequest = new HttpRequest();
+  Map<String, String> bytes;
+  List<String> languages;
+
+  String classFor(String language) {
+    final languageSize = bytes[language];
+    if (languageSize < 10000) return 'xs';
+    if (languageSize < 100000) return 's';
+    if (languageSize < 1000000) return 'm';
+    if (languageSize < 10000000) return 'l';
+    return 'xl';
+  }
 
   ngOnInit() {
     final path = 'packages/kipsu_fi/cron/languages';
+    final httpRequest = new HttpRequest();
     httpRequest
       ..open('GET', path)
-      ..onLoadEnd.listen((_) => onLanguagesLoaded())
+      ..onLoadEnd.listen((_) => _onLanguagesLoaded(httpRequest))
       ..send('');
   }
 
-  void onLanguagesLoaded() {
+  void _onLanguagesLoaded(httpRequest httpRequest) {
     if (httpRequest.status == 200) {
-      final data = JSON.decode(httpRequest.responseText);
-      data.forEach((k, v) {
+      bytes = JSON.decode(httpRequest.responseText);
+      languages = bytes.keys.where((k) => bytes[k] > 1000).toList();
+      languages.sort((a, b) => bytes[b].compareTo(bytes[a]));
+      bytes.forEach((k, v) {
         print('$k: $v');
       });
+      print(languages);
     }
   }
 }
