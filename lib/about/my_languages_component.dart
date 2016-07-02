5b096827368aa7caef69f4c398b475ef113e6efd
Verna Koskinen <darth.kipsu@gmail.com>
2016-07-02 13:57:28 +0300

    display logos for languages when available

--- lib/about/my_languages_component.dart
+++ lib/about/my_languages_component.dart
@@ -9,17 +9,35 @@ import 'package:angular2/router.dart';
     templateUrl: 'my_languages_component.html',
     styleUrls: const ['my_languages_component.css'])
 class MyLanguagesComponent implements OnInit {
-  Map<String, String> bytes;
+  Map<String, String> _logos = {
+    'JavaScript': 'javascript.svg',
+    'CSS': 'css.svg',
+    'Java': 'java.svg',
+    'HTML': 'html.svg',
+    'Ruby': 'ruby.svg',
+    'Clojure': 'clojure.svg',
+    'Python': 'python.svg',
+    'C': 'c.png',
+    'Dart': 'dart.svg',
+    'Scala': 'scala.svg'
+  };
+  Map<String, String> _bytes;
   List<String> languages;
 
   String classFor(String language) {
-    final languageSize = bytes[language];
+    final languageSize = _bytes[language];
     if (languageSize < 10000) return 'xs';
     if (languageSize < 100000) return 's';
     if (languageSize < 1000000) return 'm';
     if (languageSize < 10000000) return 'l';
     return 'xl';
   }
+
+  String bytes(String language) => (_bytes[language] / 1000).floor();
+
+  bool hasLogo(String language) => _logos[language] != null;
+
+  String logoUrl(String language) => 'packages/kipsu_fi/assets/logos/${_logos[language]}';
 
   ngOnInit() {
     final path = 'packages/kipsu_fi/cron/languages';
@@ -32,10 +50,10 @@ class MyLanguagesComponent implements OnInit {
 
   void _onLanguagesLoaded(httpRequest httpRequest) {
     if (httpRequest.status == 200) {
-      bytes = JSON.decode(httpRequest.responseText);
-      languages = bytes.keys.where((k) => bytes[k] > 1000).toList();
-      languages.sort((a, b) => bytes[b].compareTo(bytes[a]));
-      bytes.forEach((k, v) {
+      _bytes = JSON.decode(httpRequest.responseText);
+      languages = _bytes.keys.where((k) => _bytes[k] > 1000).toList();
+      languages.sort((a, b) => _bytes[b].compareTo(_bytes[a]));
+      _bytes.forEach((k, v) {
         print('$k: $v');
       });
       print(languages);
