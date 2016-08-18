5819ddf3b250b19a7dfb2643ed95e848c12ce5e5
Verna Koskinen <darth.kipsu@gmail.com>
2016-08-18 10:08:22 +0300

    add initial europe component to photography

---
+++ lib/photography/europe_component.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+@Component(
+    selector: 'europe-map',
+    templateUrl: 'europe_component.html',
+    styleUrls: const ['europe_component.css'])
+class EuropeComponent {
+
+}
+
--- lib/photography/photography_component.dart
+++ lib/photography/photography_component.dart
@@ -3,12 +3,14 @@ import 'dart:html';
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
+import 'package:kipsu_fi/photography/europe_component.dart';
 import 'package:kipsu_fi/services/title_service.dart';
 
 @Component(
     selector: 'photography',
     templateUrl: 'photography_component.html',
-    styleUrls: const ['photography_component.css'])
+    styleUrls: const ['photography_component.css'],
+    directives: const [EuropeComponent])
 class PhotographyComponent implements OnActivate {
   final TitleService _titleService;
 
